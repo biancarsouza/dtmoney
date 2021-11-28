89c01d752aa8957afc8718adcb6b1a48c88a2256
Bianca <biancaridesouza@gmail.com>
2021-11-28 19:59:21 -0300

    Create hook

--- src/index.tsx
+++ src/index.tsx
@@ -15,19 +15,19 @@ createServer ({
       transactions: [
         {
           id: 1,
-          title: 'Freelance de website',
-          type: 'deposit',
-          category: 'Dev',
-          amount: 6000,
-          createdAt: new Date('2021-02-12 09:00:00'),
+          title: 'Crunchyroll',
+          type: 'withdraw',
+          category: 'Streaming',
+          amount: 30,
+          createdAt: new Date('2021-11-28 09:00:00'),
         },
         {
           id: 2,
-          title: 'Aluguel',
-          type: 'withdraw',
-          category: 'Casa',
-          amount: 1100,
-          createdAt: new Date('2021-02-14 11:00:00'),
+          title: 'Revenda one piece',
+          type: 'deposit',
+          category: 'Mangás',
+          amount: 150,
+          createdAt: new Date('2021-11-03 11:00:00'),
         }
       ],
     })
